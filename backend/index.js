376be00d018ac69e01f6134d456896e3e5d92fcb
Niels <nielsvdschepop12367@gmail.com>
2025-01-27 03:54:27 -0800

    .

--- backend/index.js
+++ backend/index.js
@@ -21,9 +21,12 @@ app.use(cors({
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('Connected to MongoDB!'))
-  .catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB!'))
+.catch(err => console.error('MongoDB connection error:', err));
 
 // Routes (AFTER CORS and middleware)
 app.use('/api/auth', authRoutes);
