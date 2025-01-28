41b8dd272d8c4e739df3b91932b7e0158b3e4701
Niels <nielsvdschepop12367@gmail.com>
2025-01-27 20:27:17 -0800

    pwa app

---
+++ frontend/next.config.js
@@ -0,0 +1,13 @@
+// next.config.js
+import withPWA from 'next-pwa'
+
+const nextConfig = withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development'
+})({
+  reactStrictMode: true,
+});
+
+export default nextConfig;
