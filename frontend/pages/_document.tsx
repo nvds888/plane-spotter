ede65de9e80a4a345a2e978ccd2ebf6b26097a0e
Niels <nielsvdschepop12367@gmail.com>
2025-01-27 20:40:44 -0800

    .

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,28 +1,14 @@
-// pages/_document.tsx
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html>
       <Head>
-        {/* PWA meta tags */}
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="application-name" content="Plane Spotter" />
-        <meta name="apple-mobile-web-app-title" content="Plane Spotter" />
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="theme-color" content="#3b82f6" />
-        <meta name="msapplication-navbutton-color" content="#3b82f6" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-        <meta name="msapplication-starturl" content="/" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-
-        {/* PWA manifest */}
         <link rel="manifest" href="/manifest.json" />
-
-        {/* Icons */}
-        <link rel="icon" type="image/png" sizes="192x192" href="/icons/icon-192x192.png" />
-        <link rel="icon" type="image/png" sizes="512x512" href="/icons/icon-512x512.png" />
-        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
       </Head>
       <body>
         <Main />
