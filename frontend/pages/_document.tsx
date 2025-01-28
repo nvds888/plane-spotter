8ca579d9c8bb2d7261b60fa7bc4af5c36e7c1ade
Niels <nielsvdschepop12367@gmail.com>
2025-01-27 20:36:55 -0800

    .

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,25 +1,28 @@
+// pages/_document.tsx
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html>
       <Head>
-        <meta name="application-name" content="Plane Spotter" />
+        {/* PWA meta tags */}
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="application-name" content="Plane Spotter" />
         <meta name="apple-mobile-web-app-title" content="Plane Spotter" />
-        <meta name="description" content="Track and identify planes in your area" />
         <meta name="theme-color" content="#3b82f6" />
-        
+        <meta name="msapplication-navbutton-color" content="#3b82f6" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="msapplication-starturl" content="/" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+        {/* PWA manifest */}
         <link rel="manifest" href="/manifest.json" />
+
+        {/* Icons */}
+        <link rel="icon" type="image/png" sizes="192x192" href="/icons/icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/icons/icon-512x512.png" />
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
-        
-        {/* Add splash screen for iOS */}
-        <link
-          href="/icons/splash-screen.png"
-          media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
-          rel="apple-touch-startup-image"
-        />
       </Head>
       <body>
         <Main />
