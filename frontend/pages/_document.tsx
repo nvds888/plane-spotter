a7304c061d2e89b09582ce4c21d6c867e7f1fd8d
Niels <nielsvdschepop12367@gmail.com>
2025-02-07 00:31:21 -0800

    .

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -9,6 +9,7 @@ export default function Document() {
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="Planeify" />
+        <meta name="theme-color" content="#4F46E5" />
         <link rel="manifest" href="/manifest.json" />
       </Head>
       <body className="antialiased">
