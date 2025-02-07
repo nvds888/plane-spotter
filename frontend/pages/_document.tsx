8d84b6f48d28b87a083706eaadc645d910264502
Niels <nielsvdschepop12367@gmail.com>
2025-02-07 00:08:24 -0800

    icon logo

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -5,10 +5,18 @@ export default function Document() {
     <Html lang="en">
       <Head>
         {/* Keep your original setup */}
-        <meta name="application-name" content="Plane Spotter" />
+        <meta name="application-name" content="Planeify" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-title" content="Plane Spotter" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="Planeify" />
         <meta name="theme-color" content="#3850d5" />
+        
+        {/* Add these lines for iOS icon support */}
+        <link rel="apple-touch-icon" href="/icons/apple-icon-180x180.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-icon-180x180.png" />
+        <link rel="apple-touch-icon" sizes="167x167" href="/icons/apple-icon-167x167.png" />
+        
         <link rel="manifest" href="/manifest.json" />
       </Head>
       <body className="antialiased">
