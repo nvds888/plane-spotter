dea5c97f08a704f797cf915c5df881b906d0868d
Niels <nielsvdschepop12367@gmail.com>
2025-02-07 00:13:42 -0800

    .

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -4,20 +4,29 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Keep your original setup */}
-        <meta name="application-name" content="Planeify" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-        <meta name="apple-mobile-web-app-title" content="Planeify" />
-        <meta name="theme-color" content="#3850d5" />
-        
-        {/* Add these lines for iOS icon support */}
-        <link rel="apple-touch-icon" href="/icons/apple-icon-180x180.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-icon-152x152.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-icon-180x180.png" />
-        <link rel="apple-touch-icon" sizes="167x167" href="/icons/apple-icon-167x167.png" />
-        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <meta name="format-detection" content="telephone=no" />
+
+        {/* PWA tags */}
         <link rel="manifest" href="/manifest.json" />
+        
+        {/* iOS specific tags */}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Planeify" />
+        
+        {/* iOS icons - make sure all these sizes exist */}
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-touch-icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon-180x180.png" />
+        <link rel="apple-touch-icon" sizes="167x167" href="/icons/apple-touch-icon-167x167.png" />
+
+        {/* iPhone Splash Screen Images */}
+        <link rel="apple-touch-startup-image" href="/splash/apple-splash-2048-2732.png" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)" />
+        <link rel="apple-touch-startup-image" href="/splash/apple-splash-1668-2388.png" media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)" />
+        <link rel="apple-touch-startup-image" href="/splash/apple-splash-1536-2048.png" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)" />
+        <link rel="apple-touch-startup-image" href="/splash/apple-splash-1125-2436.png" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" />
+        <link rel="apple-touch-startup-image" href="/splash/apple-splash-1242-2688.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)" />
       </Head>
       <body className="antialiased">
         <Main />
