83864e6f812dfa924ad8b54f0fe7f17974f58ff3
Niels <nielsvdschepop12367@gmail.com>
2025-02-09 20:45:45 -0800

    .

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -4,10 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-  {/* Base theme color for browser */}
-  <meta name="theme-color" media="(prefers-color-scheme: light)" content="#4F46E5" />
-  <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#4F46E5" />
-  
   {/* PWA configurations */}
   <meta name="apple-mobile-web-app-capable" content="yes" />
   <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
